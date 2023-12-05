f21c86d3acb59e46624c21ef720ca60a8d57aa85
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-12-04 19:39:14 -0500

    Matriz Tridimensional Finalizada

--- tridi.c
+++ tridi.c
@@ -7,6 +7,7 @@ usuario. El programa debe inicializar todas las matrices del arreglo en ceros, a
 
 int main()
 {
+    // Variables a usar
     int fila = 0, columna = 0, profundidad = 0;
 
     printf("***** ARREGLO TRIDIMENSIONAL ****\n");
@@ -17,6 +18,7 @@ int main()
     printf("Ingrese la dimension de 'profundidad':\n");
     scanf("%d", &profundidad);
 
+    // Definimos la matriz con sus 3 parametros
     int MatrizTridi[profundidad][fila][columna];
 
     for (int k = 0; k < profundidad; k++)
@@ -25,14 +27,21 @@ int main()
         {
             for (int j = 0; j < columna; j++)
             {
+                // Se definel valor de cero para la matriz
+                if (k == (profundidad-1))
+                {
+                    MatrizTridi[k][i][j] = 1;
+                }else{
+                
                 MatrizTridi[k][i][j] = 0;
+                }
+                // Se usa el bucle for para imprimir directamente la matriz
                 printf("%4d |", MatrizTridi[k][i][j]);
             }
-            printf("\n");            
+            printf("\n");
         }
         printf("\n");
     }
-    
-    
+
     return 0;
 }
